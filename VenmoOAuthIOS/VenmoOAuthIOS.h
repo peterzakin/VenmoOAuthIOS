af849566af47f7f2670d9c5a1ae5b8c07e697d2f
Peter Zakin <peter.zakin@gmail.com>
2013-09-23 18:51:15 -0400

    adds xcode proj

---
+++ VenmoOAuthIOS/VenmoOAuthIOS.h
@@ -0,0 +1,13 @@
+//
+//  VenmoOAuthIOS.h
+//  VenmoOAuthIOS
+//
+//  Created by Peter Zakin on 9/23/13.
+//  Copyright (c) 2013 Venmo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VenmoOAuthIOS : NSObject
+
+@end
---
+++ VenmoOAuthIOS/VenmoOAuthIOS.m
@@ -0,0 +1,13 @@
+//
+//  VenmoOAuthIOS.m
+//  VenmoOAuthIOS
+//
+//  Created by Peter Zakin on 9/23/13.
+//  Copyright (c) 2013 Venmo. All rights reserved.
+//
+
+#import "VenmoOAuthIOS.h"
+
+@implementation VenmoOAuthIOS
+
+@end
