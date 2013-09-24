ab6da5f048492e54d5d24a7ec6b676ac9a38f46e
Ben Guo <ben@venmo.com>
2013-09-24 15:51:25 -0400

    new test classes

---
+++ VenmoOAuthIOSTests/VENClientTests.m
@@ -0,0 +1,34 @@
+//
+//  VENClientTest.m
+//  VenmoOAuthIOS
+//
+//  Created by Ben Guo on 9/24/13.
+//  Copyright (c) 2013 Venmo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VENClientTests : XCTestCase
+
+@end
+
+@implementation VENClientTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExtractTokenFromServerSideFlow
+{
+    
+}
+
+@end
